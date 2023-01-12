a2cddcffaf7578d7993a71064aae0a18810df11f
Strazzeri.Steve <pro.stevestrz@gmail.com>
2023-01-12 12:01:03 +0100

    Basic routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import Home from './components/Home/Home';
+import Ranking from './components/Ranking/Ranking';
+import Tournaments from './components/Tournaments/Tournaments';
+import Authentication from './components/Authentication/Authentication';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Tournaments" element={<Tournaments />} />
+            <Route path="/Ranking" element={<Ranking />} />
+            <Route path='/Authentication' element={<Authentication />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
