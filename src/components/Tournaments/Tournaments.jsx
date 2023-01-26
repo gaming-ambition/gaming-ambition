e5fefe615a9bc05df4aa3ba33494ab66c3f0b0cf
Strazzeri.Steve <pro.stevestrz@gmail.com>
2023-01-26 14:59:58 +0100

    tournament

--- src/components/Tournaments/Tournaments.jsx
+++ src/components/Tournaments/Tournaments.jsx
@@ -1,6 +1,21 @@
+import './Tournaments.css'
+import codBg from '../../img/cod_bg.jpeg';
+import fifaBg from '../../img/fifa_bg.jpeg';
+
 function Tournaments() {
   return (
-    <p>Tournaments</p>
+    <>
+      <div id="header">
+        <p>CURRENT TOURNAMENTS</p>
+      </div>
+      <div id="tournaments">
+        <div id="game1" style={{ backgroundImage: `url(${codBg}`}}>
+          
+        </div>
+        <div id="game2" style={{ backgroundImage: `url(${fifaBg}`}}>
+        </div>
+      </div>
+    </>
   )
 }
 
